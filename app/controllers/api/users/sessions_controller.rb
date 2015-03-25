916d4fe0264550ed80f9084dbabcc60583bf1910
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2015-03-24 21:50:14 -0300

    Ahora anda el login/logout con la API

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -8,9 +8,53 @@ class Api::Users::SessionsController < ApplicationController
 
   # POST /resource/sign_in
   def create
-    logger.info "[API] Session Create #{params}"
-    #super
-    render json: {:status => "OK"}, status: :ok # :bad_request
+    logger.debug "[API] Session Create: #{params}"
+    # Fetch params
+    email = params[:session][:email] if params[:session]
+    password = params[:session][:password] if params[:session]
+
+    id = User.find_by(email: email).try(:id) if email.presence
+
+    # Validations
+    if request.format != :json
+      render status: :not_acceptable, json: { message: 'The request must be JSON.' }
+      return
+    end
+
+    if email.nil? or password.nil?
+      render status: :bad_request, json: { message: 'The request MUST contain the user email and password.' }
+      return
+    end
+
+    # Authentication
+    user = User.find_by(email: email)
+
+    if user
+      if user.valid_password? password
+        user.restore_authentication_token!
+        # Note that the data which should be returned depends heavily of the API client needs.
+        render status: :ok, json: { email: user.email, user_token: user.authentication_token }
+      else
+        render status: :unauthorized, json: { message: 'Invalid email or password.' }
+      end
+    else
+      render status: :unauthorized, json: { message: 'Invalid email or password.' }
+    end
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    logger.debug "[API] Session Destroy: #{params}"
+    # Fetch params
+    user = User.find_by(authentication_token: params[:user_token])
+
+    if user.nil?
+      render status: :not_found, json: { message: 'Invalid token.' }
+    else
+      user.authentication_token = nil
+      user.save!
+      render status: :ok, json: nil
+    end
   end
 
 end
