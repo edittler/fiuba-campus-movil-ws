b636cdc5440110f1eccf2a333c2ecbf737abe4a8
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2015-04-10 23:29:36 -0300

    Agrego validaciones a la creacion de usuarios

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -3,22 +3,43 @@ class Api::Users::RegistrationsController < ApplicationController
 
   # POST /api/users/sign_up
   def create
-    logger.debug "[API] User Create #{params}"
+    # Check correct params
+    unless exists_required_params(params[:registration])
+      render status: :bad_request,
+             json: { result: "error", message: "Missing required parameters" }
+      return
+    end
 
+    # Check exist user e-mail
+    if User.find_by_email(params[:registration][:email])
+      render status: :conflict,
+             json: { result: "error", message: "User already exist" }
+      return
+    end
+
+    # Create user
     user = User.new(user_params)
     if user.save
-      render status: :created, json: { sucess: true}
+      render status: :created,
+            json: { result: "ok", message: "User has been created" }
       return
     else
       warden.custom_failure!
-      render status: :unprocessable_entity, :json => user.errors
+      render status: :bad_request,
+             json: { result: "error", message: "An error has occurred", errors: user.errors }
     end
   end
 
-  def user_params
-    params[:user] = params[:registration]
-    logger.debug "[API] User Params #{params}"
-    params.require(:user).permit(:email, :password)
-  end
+  private
+
+    def exists_required_params(registration_params)
+      logger.debug "[API] User Create, check params #{registration_params}"
+      return ( !params[:email].nil? and !params[:password].nil? )
+    end
+
+    def user_params
+      logger.debug "[API] User Params #{params}"
+      params.require(:registration).permit(:email, :password)
+    end
 
 end
