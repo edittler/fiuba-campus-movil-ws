31dd67232416801b80aead15ed508ffdd70c8bff
TomR <tomasreale@gmail.com>
2015-04-12 22:12:52 -0300

    Fix errores de getProfile. EditProfile in progress

--- app/controllers/api/users/profile_controller.rb
+++ app/controllers/api/users/profile_controller.rb
@@ -2,13 +2,45 @@ class Api::Users::ProfileController < Api::ApiController
 
   def show
 
-    @user = User.find(params[:id].to_i)
-    @nationality = Nationality.find( @user.profile.nationality_id )   if  !@user.profile.nationality_id.nil?
-    @city = City.find( @user.profile.city_id )                        if  !@user.profile.city_id.nil?
+    @profile = Profile.find_by!( user_id: params[:id].to_i )
 
-    @jobs = Job.find_by( profile_id: @user.profile_id )
+    @nationality = Nationality.find_by!( profile_id: @profile.id )
 
-    logger.debug "[API] Show User: #{@user.attributes.inspect}"
+    @city = City.find_by!( profile_id: @profile.id )
+
+    @phone = Phone.find_by!( profile_id: @profile.id )
+
+    @jobs = Job.find_by( profile_id: @profile.id )
+
+    logger.debug "[API] Show User: #{@profile.attributes.inspect}"
+  end
+
+  def edit
+
+    @profile = Profile.find_by!( user_id: params[:id].to_i )
+
+    @profile.first_name = params[:first_name]
+    @profile.last_name = params[:last_name]
+    @profile.biography = params[:biography]
+    @profile.save
+
+    @nationality = Nationality.find_by!( profile_id: @profile.id )
+    @nationality.nationality = params[:nationality]
+    @nationality.save
+
+    @city = City.find_by!( profile_id: @profile.id )
+    @city.name = params[:city]
+    @city.save
+
+    @phone = Phone.find_by!( profile_id: @profile.id )
+    @phone.number = params[:phone]
+    @phone.save
+
+    @jobs = Job.find_by( profile_id: @profile.id )
+    render status: :created,
+            json: { result: "ok", message: "Profile has been updated" }
+
+
   end
 
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -6,6 +6,11 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  profile_id :integer
+#
+# Indexes
+#
+#  index_cities_on_profile_id  (profile_id)
 #
 
 require 'rails_helper'
