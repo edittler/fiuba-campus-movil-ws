2c3d637170576c9ffe946ba878734868429a7593
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2015-05-31 15:18:34 -0300

    Elimino un return que no va en el controller de comentarios del muro. Indento el codigo del controller

--- app/controllers/api/users/wall/comments_controller.rb
+++ app/controllers/api/users/wall/comments_controller.rb
@@ -1,65 +1,72 @@
 class Api::Users::Wall::CommentsController < Api::ApiController
 
-	#GET /api/users/:id/wall/comments
-	def show
-    	wallOwnerUser = User.find_by(id: params[:id].to_i)
-		wall = wallOwnerUser.wall
-		if wallOwnerUser.nil?
-			render status: :error,
-	       json: { result: "error", message: "User does not exist" }
-	    	return
-		end
+  # GET /api/users/:id/wall/comments
+  def show
+    wallOwnerUser = User.find_by(id: params[:id].to_i)
 
-		if wallOwnerUser.wall.nil?
-			wallOwnerUser.wall = Wall.create(user_id: params[:id].to_i)
-	    	return
-		end
+    if wallOwnerUser.nil?
+      render status: :error,
+             json: { result: "error", message: "User does not exist" }
+      return
+    end
 
-		if wallOwnerUser.wall.comments.nil?
-			render status: :ok,
-	         json: {result: "ok", message: "No comments to show", data: { wallComments: [] } }
-	  		return
-		end
-		@comments = wallOwnerUser.wall.comments
-		render status: :ok
-	end
+    if wallOwnerUser.wall.nil?
+      wallOwnerUser.wall = Wall.create(user_id: params[:id].to_i)
+    end
 
-	#POST /api/users/:id/wall/comments
-	def create_wall_comment
-		unless exists_create_comment_required_params?
-      		render_missing_required_params
-      		return
-    	end
+    if wallOwnerUser.wall.comments.nil?
+      render status: :ok,
+             json: {
+                      result: "ok",
+                      message: "No comments to show",
+                      data: { wallComments: [] }
+                   }
+      return
+    end
 
-    	creatorUser = User.find_by_authentication_token(params[:user_token])
-    	wallOwnerUser = User.find_by(id: params[:id].to_i)
-		wall = wallOwnerUser.wall
-		
-		if creatorUser.nil?
-	  		render status: :error,
-	           json: { result: "error", message: "User does not exist" }
-	        return
-    	end	
+    @comments = wallOwnerUser.wall.comments
+    render status: :ok
+  end
 
-    	if wall.nil?
-	  		render status: :error,
-	           json: { result: "error", message: "Wall does not exist" }
-	        return
-    	end
+  # POST /api/users/:id/wall/comments
+  def create_wall_comment
+    unless exists_create_comment_required_params?
+      render_missing_required_params
+      return
+    end
 
-		comment = Comment.create(text: params[:message])
-		comment.user = creatorUser 	
-    	wall.comments << comment
+    creatorUser = User.find_by_authentication_token(params[:user_token])
 
-		render status: :created,
+    if creatorUser.nil?
+      render status: :error,
+             json: { result: "error", message: "User does not exist" }
+      return
+    end
+
+    wallOwnerUser = User.find_by(id: params[:id].to_i)
+    wall = wallOwnerUser.wall
+
+    if wall.nil?
+      render status: :error,
+             json: { result: "error", message: "Wall does not exist" }
+      return
+    end
+
+    comment = Comment.create(text: params[:message])
+    comment.user = creatorUser
+    wall.comments << comment
+
+    render status: :created,
            json: { result: "ok", message: "Create comment success" }
-	end
+  end
 
-private
-	def exists_create_comment_required_params?
+  private
+
+    def exists_create_comment_required_params?
       if params[:message].nil?
-      	return false
+        return false
       end
       return true
     end
+
 end
