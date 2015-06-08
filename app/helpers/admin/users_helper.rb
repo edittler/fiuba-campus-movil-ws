eee806e154084d5f77c9f33c351162067d3316fd
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2015-06-08 01:11:37 -0300

    mejoro el estilo visual dentro del administrador

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,5 +1,25 @@
 module Admin::UsersHelper
 
+  def user_full_name(user)
+    "#{user.profile.first_name} #{user.profile.last_name}"
+  end
+
+  def user_padron(user)
+    "#{user.academic_info.padron}"
+  end
+
+  def user_career(user)
+    "#{user.academic_info.carreer}"
+  end
+
+  def approve_user_status(user)
+    if user.approved
+      "Si"
+    else
+      "No"
+    end
+  end
+
   def approve_user(user)
     if user.approved
       "Reenviar mail"
