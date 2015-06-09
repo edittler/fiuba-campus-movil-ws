2fbd01426e405a1ca51ff1e55e7fa192ee1471ed
Marco Lotto <marcol91@gmail.com>
2015-06-09 20:18:37 -0300

    Cambios en una migration de imagen de grupo

--- db/migrate/20150609214524_add_picture_to_group.rb
+++
@@ -1,8 +0,0 @@
-class AddPictureToGroup < ActiveRecord::Migration
-  def change
-    add_column :groups, :picture_file_name, :string
-    add_column :groups, :picture_content_type, :string
-    add_column :groups, :picture_file_size, :integer
-    add_column :groups, :picture_updated_at, :datetime
-  end
-end
