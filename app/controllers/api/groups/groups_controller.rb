1240a8df5df766e3b5790d6cc60babcaa3084347
Marco Lotto <marcol91@gmail.com>
2015-06-09 19:41:22 -0300

    Prueba

--- app/controllers/api/groups/groups_controller.rb
+++ app/controllers/api/groups/groups_controller.rb
@@ -34,6 +34,8 @@ class Api::Groups::GroupsController < Api::ApiController
       return
     end
 
+    puts "PUERCO1"
+
     myUser = User.find_by_authentication_token(params[:user_token])
     group = Group.find_by(id: params[:id].to_i)
     if group.nil?
@@ -42,15 +44,21 @@ class Api::Groups::GroupsController < Api::ApiController
         return
     end
 
+    puts "PUERCO2"
+
     if !myUser.in_group?(group)
       render status: :conflict,
              json: {result: "error", message: "User is not in the group"}
       return
     end
 
+    puts "PUERCO3"
+
     begin
       params[:image] = parse_image_data(params[:image]) if params[:image]
       group.picture = params[:image]
+
+      puts "PUERCO4"
       
       result = {
         result: "error",
@@ -62,11 +70,15 @@ class Api::Groups::GroupsController < Api::ApiController
         render status: :ok, json: result.to_json
         return
       end
+    puts "PUERCO5"
     rescue Exception => e
       logger.error "#{e.message}"
     end
 
+    puts "PUERCO6"
+
     render status: :internal_server_error, json: result.to_json
+    puts "PUERCO7"
   ensure
     clean_tempfile
   end
@@ -85,24 +97,29 @@ class Api::Groups::GroupsController < Api::ApiController
     end
 
     def exists_upload_picture_required_params
+      puts "PUERCO11"
       image_params = params[:image]
       return false if image_params.nil?
+      puts "PUERCO12"
       return !( image_params[:filename].nil? or image_params[:content].nil? or image_params[:content_type].nil? )
     end
 
     def parse_image_data(image_data)
+      puts "PUERCO21"
       @tempfile = Tempfile.new('item_image')
       @tempfile.binmode
       @tempfile.write Base64.decode64(image_data[:content])
       @tempfile.rewind
+      puts "PUERCO22"
 
       uploaded_file = ActionDispatch::Http::UploadedFile.new(
         tempfile: @tempfile,
         filename: image_data[:filename]
       )
-
+      puts "PUERCO23"
       uploaded_file.content_type = image_data[:content_type]
       uploaded_file
+      puts "PUERCO24"
     end
 
     def clean_tempfile
